519b69da6652bd6d23f1beb39922162d6f5dda39
Maiananthan <46962558+maiananthan@users.noreply.github.com>
2023-04-17 23:14:45 +0530

    cli option addition

--- testProgs/testMPEG2TransportStreamer.cpp
+++ testProgs/testMPEG2TransportStreamer.cpp
@@ -18,6 +18,13 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 // and streams it using RTP
 // main program
 
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <vector>
+
 #include "liveMedia.hh"
 
 #include "BasicUsageEnvironment.hh"
@@ -45,17 +52,90 @@ Boolean const isSSM = False;
 // The product of these two numbers must be enough to fit within a network packet
 
 UsageEnvironment* env;
-char const* inputFileName = "test.ts";
+// char const* inputFileName = "test.ts";
+char *inputFileName;
+char *destinationAddressStr;
+unsigned short rtpPortNum;
+long count = 1;
 FramedSource* videoSource;
 RTPSink* videoSink;
 
 void play(); // forward
 
+#define DEBUG 0
+#if DEBUG == 1
+#define LOG_D(fmt, ...) printf("%s[%d]: " fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#define LOG_D(fmt, ...)
+#endif
+#define LOG_I(fmt, ...) printf(fmt, ##__VA_ARGS__);
+#define LOG_E(fmt, ...) printf("\e[1;31m" fmt "\e[0m", ##__VA_ARGS__)
+
+class InputParser{
+    public:
+        InputParser (int &argc, char **argv){
+            for (int i=1; i < argc; ++i)
+                this->tokens.push_back(std::string(argv[i]));
+        }
+        /// @author iain
+        const std::string& getCmdOption(const std::string &option) const{
+            std::vector<std::string>::const_iterator itr;
+            itr =  std::find(this->tokens.begin(), this->tokens.end(), option);
+            if (itr != this->tokens.end() && ++itr != this->tokens.end()){
+                return *itr;
+            }
+            static const std::string empty_string("");
+            return empty_string;
+        }
+        /// @author iain
+        bool cmdOptionExists(const std::string &option) const{
+            return std::find(this->tokens.begin(), this->tokens.end(), option)
+                != this->tokens.end();
+        }
+    private:
+        std::vector <std::string> tokens;
+};
+
 int main(int argc, char** argv) {
   // Begin by setting up our usage environment:
   TaskScheduler* scheduler = BasicTaskScheduler::createNew();
   env = BasicUsageEnvironment::createNew(*scheduler);
+  InputParser parse_arg(argc, argv);
+  if(parse_arg.cmdOptionExists("-h")) {
+    LOG_I("Usage: %s -[f|m|p|h] [VALUES]\n", argv[0]);
+    LOG_I("       -f file name\n");
+    LOG_I("       -m multicast address\n");
+    LOG_I("       -p port\n");
+    LOG_I("       -h this help message\n");
+    exit(0);
+  }
+  if(parse_arg.cmdOptionExists("-f")) {
+    inputFileName = (char *) malloc(strlen( parse_arg.getCmdOption("-f").c_str() ) + 1 );
+    // strcpy(inputFileName, argv[1]);
+    strcpy(inputFileName, parse_arg.getCmdOption("-f").c_str());
+    LOG_D("inputFileName: \'%s\'\n", inputFileName);
+  } else {
+    LOG_E("missing option: -f\n");
+    LOG_E("Try \'%s -h\' for information/usage\n", argv[0]);
+  }
+  if(parse_arg.cmdOptionExists("-m")) {
+    destinationAddressStr = (char *) malloc( strlen( parse_arg.getCmdOption("-m").c_str() ) + 1 );
+    strcpy(destinationAddressStr, parse_arg.getCmdOption("-m").c_str());
+    LOG_D("multicast address: %s\n", destinationAddressStr);
+  } else {
+    LOG_E("missing option: -m\n");
+    LOG_E("Try \'%s -h\' for information/usage\n", argv[0]);
+  }
+  if(parse_arg.cmdOptionExists("-p")) {
+    rtpPortNum = (unsigned short)std::stoi(parse_arg.getCmdOption("-p"));
+    LOG_D("port number: %d\n", rtpPortNum);
+  } else {
+    LOG_E("missing option: -p\n");
+    LOG_E("Try \'%s -h\' for information/usage\n", argv[0]);
+    exit(0);
+  }
 
+#if 0
   // Create 'groupsocks' for RTP and RTCP:
   char const* destinationAddressStr
 #ifdef USE_IPV6_MULTICAST
@@ -71,12 +151,15 @@ int main(int argc, char** argv) {
     = "239.255.42.42";
 #endif
 #endif
+
+#endif
+
   // Note: This is a multicast address.  If you wish to stream using
   // unicast instead, then replace this string with the unicast address
   // of the (single) destination.  (You may also need to make a similar
   // change to the receiver program.)
 
-  const unsigned short rtpPortNum = 1234;
+  // const unsigned short rtpPortNum = 1234;
   const unsigned short rtcpPortNum = rtpPortNum+1;
   const unsigned char ttl = 7; // low, in case routers don't admin scope
 
@@ -165,5 +248,7 @@ void play() {
 
   // Finally, start playing:
   *env << "Beginning to read from file...\n";
+  LOG_I("ts file: \e[1;33m%s\e[0m [\e[1;34m%ld\e[0m]\n", inputFileName, count++);
+  LOG_I("url    : \e[1;33m%s\e[0m:\e[1;33m%d\e[0m\n", destinationAddressStr, rtpPortNum);
   videoSink->startPlaying(*videoSource, afterPlaying, videoSink);
 }
